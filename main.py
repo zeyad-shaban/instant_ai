ed7a272ec7d7d326ddc5d57d5a2a8cd63dcfea1d
zeyad-shaban <zeyadshapan2004@gmail.com>
2026-05-03 06:01:44 +0300

    added welcoming message ai generated

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from google import genai
 
 app = FastAPI()
+client = genai.Client()
 
 
 @app.get("/")
-def home():
-    return {
-        "message": "Hello World welcome to the great AI course!",
-    }
+def root():
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-lite-preview",
+        contents="you are an AI model running on a live server that was just deployed, welcome the user with a short excited message letting him know this is new to production server",
+    )
+    
+    response_text = response.text.replace("n", '</br>') # type: ignore
+    
+    return (
+        f"""
+        <html>
+            <body>
+                <h1>Welcome to this app!</h1>
+                <p>{response_text}</p>
+            </body>
+        </html>
+        """
+    )
+    
